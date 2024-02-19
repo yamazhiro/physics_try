f36a67aae1c2c99edc0394f6e15de9fc82850306
yamazhiro <yahi140902kabu@gmail.com>
2024-02-19 14:50:32 +0900

    [Add] pitching_delta_sub

--- pitching_delta.py
+++ pitching_delta.py
@@ -7,14 +7,15 @@ def plot(results):
     fig, ax = plt.subplots(figsize=(6, 6), constrained_layout=True)
     for r in results:
         # namedtupleの各フィールドにはドットでアクセス
-        ax.plot(r.yt, r.zt, marker='.',markersize=10, label=r.key)
-    plt.grid(which='major',lw=0.4)
+        ax.plot(r.yt, r.zt, marker='.',markersize=20, label=r.key)
+    plt.grid(which='major',lw=0.7)
+    plt.grid(which='minor',lw=0.4)
     ax.set_xlim(-0.5,0.5)
     ax.set_ylim(-0.5,0.5)
     ax.set_xlabel(r'Slide$(y-axis)$ [m]',fontsize=20)
     ax.set_ylabel(r'Hop$(z-axis)$ [m]',fontsize=20)
     ax.set_aspect('equal')
-    #ax.legend(ncol=2)
+    ax.legend(ncol=2,fontsize=11)
 
 def plot3d(results):
     fig,ax=plt.subplots(figsize=(8,4),subplot_kw={'projection':'3d'})
@@ -28,6 +29,7 @@ def plot3d(results):
     ax.set_xlim(0,18)
     ax.set_ylim(-1,1)
     ax.set_zlim(0,2)
+    fig.legend(ncol=2)
 
 def f_newton(t, X, m, g, b, c, w_x, w_y, w_z):
     # X = [x, v_x, y, v_y, z, v_z]
@@ -54,30 +56,6 @@ def solve_newton(eq_params, X0, t_range, n_t):
     
     return Xt[0, :], Xt[1, :], Xt[2, :], Xt[3, :], Xt[4, :], Xt[5, :]
 
-def x_rot(w=np.array([0,-1,0]),theta_degree=10):
-    theta = theta_degree *np.pi/180
-    w_x = w[0]
-    w_y = w[1]*np.cos(theta) - w[2]*np.sin(theta)
-    w_z = w[1]*np.sin(theta) + w[2]*np.cos(theta)
-    w_rot=np.array([w_x,w_y,w_z])
-    return w_rot
-
-def y_rot(w=np.array([0,-1,0]),theta_degree=10):
-    theta = theta_degree *np.pi/180
-    w_x = w[0]*np.cos(theta) + w[2]*np.sin(theta)
-    w_y = w[1]
-    w_z = -w[0]*np.sin(theta) + w[2]*np.cos(theta)
-    w_rot=np.array([w_x,w_y,w_z])
-    return w_rot
-
-def z_rot(w=np.array([0,-1,0]),theta_degree=10):
-    theta = theta_degree *np.pi/180
-    w_x = w[0]*np.cos(theta) - w[1]*np.sin(theta)
-    w_y = w[0]*np.sin(theta) + w[1]*np.cos(theta)
-    w_z = w[2]
-    w_rot=np.array([w_x,w_y,w_z])
-    return w_rot
-
 def pitching(theta=0,g=9.8,m=0.1,b=0.1,c=0.0001,rps=150,w=np.array([0,1,0]),v0=100.0/3.6,
              results=[],results2d=[],kijun=[],
              t_start=0,t_end=5.0,n_t=101,Result=[],Result2d=[]):
@@ -99,60 +77,53 @@ def pitching(theta=0,g=9.8,m=0.1,b=0.1,c=0.0001,rps=150,w=np.array([0,1,0]),v0=1
                                     X0=X0, t_range=(t_start, t_end), n_t=n_t)
     # 結果をnamedtupleにまとめ、リストに追加 (*2)
     for i in range(len(xt)):
-        if xt[i] > 20:
-            y = ((20-xt[i-1])*yt[i]+(xt[i]-20)*yt[i-1])/(xt[i]-xt[i-1])
-            z = ((20-xt[i-1])*zt[i]+(xt[i]-20)*zt[i-1])/(xt[i]-xt[i-1])
-            results2d.append(Result2d(str(label), y-kijun[0], z-kijun[1]))
+        if xt[i] > 16.5:
+            y = ((16.5-xt[i-1])*yt[i]+(xt[i]-16.5)*yt[i-1])/(xt[i]-xt[i-1])
+            z = ((16.5-xt[i-1])*zt[i]+(xt[i]-16.5)*zt[i-1])/(xt[i]-xt[i-1])
+            results2d.append(Result2d(str(label), y-kijun[0].y0, z-kijun[0].z0))
             break
     results.append(Result(str(label), xt, yt, zt))
 
-# マグヌス効果がないとき，x=16.5 mでのy,z座標の取得
+# 無回転での軌道の取得
 def pitching0(theta=0,g=9.8,m=0.1,b=0.1,c=0.0001,v0=100.0/3.6,
-             t_start=0,t_end=5.0,n_t=101):
+             results=[],results2d=[],kijun=[],
+             t_start=0,t_end=5.0,n_t=101,Result=[],Result2d=[],Kijun=[]):
+    label = f'rps={round(0.0,1)}, axis={[0,0,0]}'
     # X0 = [x0, v0_x, y0, v0_y, z0, v0_z]
     X0 = np.array([0, v0 * np.cos(theta), 0, 0, 2, v0 * np.sin(theta)])
     
     xt, _, yt, _, zt, _ = solve_newton(eq_params=(m, g, b, c, 0, 0, 0),
                                     X0=X0, t_range=(t_start, t_end), n_t=n_t)
+
     # 結果をnamedtupleにまとめ、リストに追加 (*2)
     for i in range(len(xt)):
-        if xt[i] > 20:
-            y = ((20-xt[i-1])*yt[i]+(xt[i]-20)*yt[i-1])/(xt[i]-xt[i-1])
-            z = ((20-xt[i-1])*zt[i]+(xt[i]-20)*zt[i-1])/(xt[i]-xt[i-1])
+        if xt[i] > 16.5:
+            y = ((16.5-xt[i-1])*yt[i]+(xt[i]-16.5)*yt[i-1])/(xt[i]-xt[i-1])
+            z = ((16.5-xt[i-1])*zt[i]+(xt[i]-16.5)*zt[i-1])/(xt[i]-xt[i-1])
+            kijun.append(Kijun(y, z))
             break
-    return y,z
+    results2d.append(Result2d(str(label), 0, 0))
+    results.append(Result(str(label), xt, yt, zt))
 
-def plot_pitching(theta=0,g=9.8,m=0.1,b=0.1,c=0.0001,N=1,theta_degree=10,
-                  rps=150,w=np.array([0,1,0]),v0=100.0/3.6,
+def plot_pitching(theta=0,g=9.8,m=0.1,b=0.1,c=0.0001,rps=150,w=np.array([0,1,0]),
+                  v0=100.0/3.6,
                   t_start=0,t_end=5.0,n_t=101,Result=[],Result2d=[],Kijun=[]):
     results = []
     results2d = []
-    y0,z0 = pitching0(theta=theta,g=g,m=m,b=b,c=c,v0=v0,t_start=t_start,t_end=t_end,n_t=n_t)
-    kijun = [y0,z0]
+    kijun = []
+    pitching0(theta=theta,g=g,m=m,b=b,c=c,v0=v0,
+              results=results,results2d=results2d,kijun=kijun,
+              t_start=t_start,t_end=t_end,n_t=n_t,Result=Result,Result2d=Result2d,Kijun=Kijun)
     
-    for i in range(N):
-        w_rot = x_rot(w=w,theta_degree=i*theta_degree)
-        pitching(theta=theta,g=g,m=m,b=b,c=c,rps=rps,w=w_rot,v0=v0,
-                results=results,results2d=results2d,kijun=kijun,
-                t_start=t_start,t_end=t_end,n_t=n_t,Result=Result,Result2d=Result2d)
-    
-    for i in range(N):
-        w_rot = x_rot(w=w,theta_degree=i*theta_degree)
-        pitching(theta=theta,g=g,m=m,b=b,c=c,rps=rps-10,w=w_rot,v0=v0,
-                results=results,results2d=results2d,kijun=kijun,
-                t_start=t_start,t_end=t_end,n_t=n_t,Result=Result,Result2d=Result2d)
-    
-    for i in range(N):
-        w_rot = x_rot(w=w,theta_degree=i*theta_degree)
-        pitching(theta=theta,g=g,m=m,b=b,c=c,rps=rps-20,w=w_rot,v0=v0,
-                results=results,results2d=results2d,kijun=kijun,
-                t_start=t_start,t_end=t_end,n_t=n_t,Result=Result,Result2d=Result2d)
-    
-    for i in range(N):
-        w_rot = x_rot(w=w,theta_degree=i*theta_degree)
-        pitching(theta=theta,g=g,m=m,b=b,c=c,rps=rps-30,w=w_rot,v0=v0,
-                results=results,results2d=results2d,kijun=kijun,
-                t_start=t_start,t_end=t_end,n_t=n_t,Result=Result,Result2d=Result2d)
+    pitching(theta=theta,g=g,m=m,b=b,c=c,rps=2700/60,w=np.array([0,-1,0]),v0=v0,
+             results=results,results2d=results2d,kijun=kijun,
+             t_start=t_start,t_end=t_end,n_t=n_t,Result=Result,Result2d=Result2d)
+    pitching(theta=theta,g=g,m=m,b=b,c=c,rps=1800/60,w=np.array([0,0,-1]),v0=v0,
+             results=results,results2d=results2d,kijun=kijun,
+             t_start=t_start,t_end=t_end,n_t=n_t,Result=Result,Result2d=Result2d)
+    pitching(theta=theta,g=g,m=m,b=b,c=c,rps=1000/60,w=np.array([0,1,0]),v0=v0,
+             results=results,results2d=results2d,kijun=kijun,
+             t_start=t_start,t_end=t_end,n_t=n_t,Result=Result,Result2d=Result2d)
     
     plot(results2d)
     plot3d(results)   #放物線の描画
@@ -164,27 +135,26 @@ def main():
     b = 0.1  # [kg/s]
     c = 0.00005 # [kg]
     
-    rps= 40
-    w = np.array([0,-1,0])
-    N = 8  # 軸の回転回数
-    theta_degree = 45 # 軸の回転角度
+    rps= 1800/60    # 回転数
+    w = np.array([0,0,1])   # 回転軸
 
     v0 = 150 * 1000 / 3600  # 初速 [m/s]
     
     t_start = 0  # 初期時刻 [s]
     t_end = 0.6  # 最終時刻 [s]
     n_t = 31  # 時刻の刻み数（グラフ描画用）
-    # 投射仰角
-    theta=0 # degree
+    
+    theta=0 # 投射仰角 [degree]
     
     # 各初期条件に対する結果をそれぞれnamedtupleとしてまとめる (*1)
     Result = namedtuple('Result', ['key', 'xt', 'yt', 'zt'])
     Result2d = namedtuple('Result2d', ['key', 'yt', 'zt'])
+    Kijun = namedtuple('Kijun', ['y0','z0'])
 
-    plot_pitching(g=g,m=m,b=b,c=c,rps=rps,w=w,N=N,theta_degree=theta_degree,
+    plot_pitching(g=g,m=m,b=b,c=c,rps=rps,w=w,
                   t_start=t_start,t_end=t_end,n_t=n_t,
-                  v0=v0,theta=theta,Result=Result,Result2d=Result2d)
-    
+                  v0=v0,theta=theta,Result=Result,Result2d=Result2d,Kijun=Kijun)
+    plt.show()
+  
 if __name__ == '__main__':
     main()
-    plt.show()
